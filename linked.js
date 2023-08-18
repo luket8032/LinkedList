bcb119bb5b3216193244b8291e2d6dd7b65657a9
Luke <luket8032@hotmail.com>
2023-08-18 12:28:03 -0500

    Complete linked list append function

--- linked.js
+++ linked.js
@@ -9,18 +9,38 @@ const linkedList = () => {
 
     const append = (newValue) => {
         const newNode = node(newValue);
-        if(head === null) {
+        if(!head) {
             head = newNode;
+            length++;
+            return head
         }
+        let pointer = head;
+        while(pointer.nextNode) {
+            pointer = pointer.nextNode;
+        }
+        pointer.nextNode = newNode;
+        tail = newNode;
+        length++;
+        return head;
     }
+
+    const prepend = (newValue) => {
+        const newNode = node(newValue);
+        head = newNode;
+        length++;
+    }   
 
     const size = () => {
         return length;
     }
 
 
-    return{append, size}
+    return{append, prepend, size, get head() { return head }}
 }
 
 const myList = linkedList();
 
+myList.append(7);
+myList.append(8);
+
+console.log(myList.head);
