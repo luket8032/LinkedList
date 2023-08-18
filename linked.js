d278edaf8ebec2baf8b012285ecbaa12f196e89e
Luke <luket8032@hotmail.com>
2023-08-18 10:58:22 -0500

    Create factory functions for node and linked list

--- linked.js
+++ linked.js
@@ -1,1 +1,17 @@
+const node = (value = null, nextNode = null) => {
+    return {value, nextNode}
+}
+
+const linkedList = () => {
+    let head = null;
+    let tail = null;
+    let length = 0;
+
+    const append = (newValue) => {
+
+    }
+
+
+    return{}
+}
 
